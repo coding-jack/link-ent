7d8a5c35a345245b903aa87d1839499098cca1c1
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-12-02 15:43:08 -0700

    Fixed typo in header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ function Header() {
       <div className="md:w-4/6 mx-auto">
         <img src="/JacksonProfile.png" className="h-24 rounded-full mx-auto mb-5" />
         <h1 className="font-bold">Jackson Music</h1>
-        <p>Listen music I'm in at any one of the links below!</p>
+        <p>Listen to music I'm in at any one of the links below!</p>
       </div>
     </header>
   );
