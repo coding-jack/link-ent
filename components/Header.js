09a6ff5ceb94eddab2e1f5fd6dbcfc81b7e552bc
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-12-01 19:55:07 -0700

    Changed header and title

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/JacksonProfile.png" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold">JFK's Music</h1>
-        <p>Listen to my music at any one of the links below!</p>
+        <h1 className="font-bold">Jackson Music</h1>
+        <p>Listen music I'm in at any one of the links below!</p>
       </div>
     </header>
   );
