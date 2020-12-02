f50248121c6dfcfc61d1ecbd77fb420552a68283
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-12-01 17:27:27 -0700

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="container flex text-center mx-auto py-10">
+      <div className="md:w-4/6 mx-auto">
+        <img src="/JacksonProfile.png" className="h-24 rounded-full mx-auto mb-5" />
+        <h1 className="font-bold">JFK's Music</h1>
+        <p>Listen to my music at any one of the links below!</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
