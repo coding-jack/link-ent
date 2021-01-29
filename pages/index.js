bc8b2216f2395549cbfc5bf6544dcc1d7ebc0995
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-01-29 00:23:47 -0700

    Adding fixed background image for mobile

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ function IndexPage() {
         />
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous"></link>
       </Head>
+      <div id="background_wrap"></div>
 
       {/* Profile Pic and Title Header*/}
       <Header />
