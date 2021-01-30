9197d0e9cdb9738657c7b6cec21312566f98e84c
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-01-30 14:41:54 -0700

    Made my name a little brighter

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/JacksonProfile.png" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold text-2xl text-green-400">Jackson Kaufman</h1>
+        <h1 className="font-bold text-2xl text-green-300">Jackson Kaufman</h1>
         <p>Check out my work</p>
         <p>at any one of the links below 🤘🏼</p>
       </div>
