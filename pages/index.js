349f2dea554459669a75af5bc21e25f08c8e0023
Jackson Kaufman <jacksonkaufman115@gmail.com>
2021-02-15 17:29:48 -0700

    Changing head description

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ function IndexPage() {
     <div className="flex flex-col min-h-screen">
       {/* FontAwesome CDN style tag */}
       <Head>
-        <title>Jackson Kaufman | LinkEnt</title>
+        <title>Jackson's Links</title>
         <meta
           name="description"
           content="🌲 A free Linktree alternative built with React and Tailwind."
