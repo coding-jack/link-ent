7de74a66c158e3a8bb51b3d3d09319626f3b30d2
Jackson Kaufman <jacksonkaufman115@gmail.com>
2025-04-21 15:50:25 -0600

    changing title

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
         <img src="/JacksonProfile.png" className="h-32 rounded-full mx-auto mb-5 border border-gray-600" />
-        <h1 className="font-bold text-3xl text-green-300">Jackson Kaufman</h1>
+        <h1 className="font-bold text-3xl text-green-300">Jackson</h1>
         <p className="text-xs">Check out my music work</p>
         <p className="text-xs">at any one of the links below 🤘🏼</p>
         <p className="pt-3 text-xs">Or take a look at my <a className="text-green-300 hover:text-green-600 duration-200" href="https://jacksonkaufman.com/">web dev site</a> I made 💻</p>
