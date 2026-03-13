50eab9f3eb6a47e33534fa8b4bb4952d2e664cac
Jackson Kaufman <jackson.kaufman.dev@gmail.com>
2026-03-12 21:14:06 -0600

    mealtrain edits

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ function Header() {
       <div className="md:w-4/6 mx-auto">
         <img src="/JacksonProfile.png" className="h-32 rounded-full mx-auto mb-5 border border-gray-600" />
         <h1 className="font-bold text-3xl text-green-300">Jackson</h1>
-        <h2 className="text-gray-400 text-sm mb-3"><a href="https://www.mealtrain.com/trains/zme4ry">MealTrain</a></h2>
-        <p className="text-xs">Bring me food when I'm recovering from Brain Surgery</p>
+        <p className="text-lg">Bring me food when I'm recovering from Brain Surgery:</p>
+        <h2 className="mb-3 text-2xl text-green-300 hover:text-green-600"><a href="https://www.mealtrain.com/trains/zme4ry">MealTrain</a></h2>
         {/* <p className="pt-3 text-xs">Or take a look at my <a className="text-green-300 hover:text-green-600 duration-200" href="https://jacksonkaufman.com/">web dev site</a> I made 💻</p> */}
       </div>
     </header>
